7eadcea212186211701f58c41795f2679e936612
Vansh <patelvansh1023@gmail.com>
2026-01-26 21:27:58 -0500

    Initial Commit

---
+++ SpeakUp/SpeakUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakUpApp.swift
+//  SpeakUp
+//
+//  Created by Vansh Patel on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeakUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
